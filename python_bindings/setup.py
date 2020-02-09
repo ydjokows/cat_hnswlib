489e92d867ff8e28e198276e10aa117cb76176f3
Andrey Vasnetsov <vasnetsov93@gmail.com>
2020-02-09 14:59:00 +0100

    Update setup.py

--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -110,6 +110,7 @@ setup(
     long_description="""hnsw""",
     ext_modules=ext_modules,
     install_requires=['pybind11>=2.0', 'numpy'],
+    setup_requires=['pybind11>=2.0', 'numpy'],
     cmdclass={'build_ext': BuildExt},
     test_suite="tests",
     zip_safe=False,
