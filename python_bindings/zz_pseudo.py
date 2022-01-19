35d609bb1383f61a595395186f345493d271d0fe
Kev. Noel <arita37@users.noreply.github.com>
2022-01-19 21:23:21 +0900

    Update zz_pseudo.py

--- python_bindings/zz_pseudo.py
+++ python_bindings/zz_pseudo.py
@@ -1,23 +1,24 @@
 
 ### Method to change is this one in the 
 
-  .knn_query2( vects, k = topk, conditions = filters   )
+  .knn_query_new( vects, k = topk, conditions = filters   )
+
+    and move the python INSIDE the C++ code.
+   
 
 
-
+  ##############################################################################################
 
     def hnsw_init(dirin="", ndim=512 , efsearch= 32):
         clientdrant = Box({})
+        
+        
         import hnswlib
         p = hnswlib.Index(space   = 'l2', dim = ndim) 
         p.load_index(dirin + "/index.bin")
         p.set_ef( efsearch)   #### Speed/precision        
 
-        df = pd_read_file(dirin + "/index.parquet")
-        df = df.set_index('idx').to_dict()
-        index  = index['id']   ## 0 --> siid 
-        indexg = index.get('genre_id', {})
-        clientdrant.p = p ; clientdrant.index = index ; clientdrant.indexg = indexg
+        clientdrant.p = p 
 
 
 
@@ -36,6 +37,11 @@
           return idxall 
         
         
+        
+        
+##########################################        
+##########################################        
+##########################################                
     #### New code    
         
     def hnsw_useremb_get_topk(vecti="2330.324,0.34234,0.2324", genreid=[2323,232,3232], topk=100, dimvect=512, filter_cond='must'):
