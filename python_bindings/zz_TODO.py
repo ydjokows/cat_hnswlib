d9c672b111e1b764daf3403670ae538558abaaf6
Kev. Noel <arita37@users.noreply.github.com>
2022-01-20 09:28:08 +0900

    Update zz_TODO.py

--- python_bindings/zz_TODO.py
+++ python_bindings/zz_TODO.py
@@ -2,6 +2,8 @@
 #################### Goal
     move the python Loop  INSIDE the C++ code binding.cpp  code
    
+    ---> Loop over the condition with only  1 vector      
+             (before only 1 condition and loop over the input vectors)
 
   
 
@@ -35,7 +37,7 @@ binding.cpp  : add new method:    knn_query_new
     features = buffer.shape[0];
          
          
-     // Loop over the condition with only  1 vector    
+     // Loop over the condition with only  1 input vector      (before only 1 condition and loop over the input vectors)
     if(normalize==false) {
         ParallelFor(0, conditions, 
                     num_threads, 
@@ -49,8 +51,11 @@ binding.cpp  : add new method:    knn_query_new
     
     
     
-  
-  
+
+                               
+                               
+#####                               
+ #### Binding usage in Python ##############################################################
   
   ##############################################################################################
   ##### Current Python version ################################################################
@@ -72,10 +77,11 @@ binding.cpp  : add new method:    knn_query_new
           
            vecti = np.array([ float(x) for x in  vecti.split(",")] ,  dtype='float32')
            idxall = [] 
-           vects  = [ vecti ]  #  [ vecti for i in range(0, len(genreid)) ] 
+                               
+           ##### Loop is in python                    
            for gi in genreid :
               filters    = [[ (False, int(gi)) ]]    ### (isexcluded:false, catid) 
-              idxallj,_  = clientdrant.p.knn_query( vects, k = topk, conditions = filters   )
+              idxallj,_  = clientdrant.p.knn_query( vecti, k = topk, conditions = filters   )
               idxall.append( idxallj[0] )
 
           return idxall 
@@ -92,10 +98,9 @@ binding.cpp  : add new method:    knn_query_new
            global clientdrant
            vecti = np.array([ float(x) for x in  vecti.split(",")] ,  dtype='float32')
            idxall = [] 
-           vects  = [ vecti ]  #  [ vecti for i in range(0, len(genreid)) ] 
 
            #### Loop is in C++                  
-           idxall  = clientdrant.p.knn_query_new( vects, k = topk, conditions = filters   )
+           idxall  = clientdrant.p.knn_query_new( vecti, k = topk, conditions = filters   )
            return idxall 
                 
         
